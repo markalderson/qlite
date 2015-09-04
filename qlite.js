0e817483243314f22f5267b7709c3b0645777c65
markalderson <m4rk4ld3rs0n@gmail.com>
2015-09-04 12:10:49 +0200

    Update :P

--- qlite.js
+++ qlite.js
@@ -3,46 +3,71 @@ var QLite = {
 		return a_value && a_value.then && typeof a_value.then === 'function';
 	},
 	deferred: function () {
+		function handleCallbackPromise(got_to_resolve, this_promise, callback_promise) {
+			var next = this_promise.next;
+			callback_promise.then(
+				// Success
+				function (next_result) {
+					if (got_to_resolve) next.resolve(next_result);
+					else next.reject(next_result);
+				},
+				// Error
+				function (next_reason) {
+					next.reject(next_reason);
+				}
+			);
+		}
+
 		return {
 			resolve: function (result) {
 				var promise = this.promise;
 				// Wrap the resolve procedure inside a function...
-				function r() {
-					var arg_for_next = promise.success_callback.call(undefined, result);
+				function res() {
 					var next = promise.next;
-					if (next) {
-						if (QLite.isPromise(arg_for_next)) {
-							arg_for_next.then(function (resolved_arg) {
-								next.resolve(resolved_arg);
-							});
-						} else next.resolve(arg_for_next);
+					var arg_for_next;
+					try {
+						arg_for_next = promise.success_callback(result);
+						if (next) {
+							if (QLite.isPromise(arg_for_next)) {
+								var got_to_resolve = true;
+								handleCallbackPromise(got_to_resolve, promise, arg_for_next);
+							} else next.resolve(arg_for_next);
+						}
+					} catch (error) {
+						if (next) next.reject(error);
 					}
 				};
-				// ...and schedule execution as soon as possible
-				setTimeout(r, 0);
+				// ...and wait until client code terminates
+				setTimeout(res, 0);
 			},
 			reject: function (reason) {
 				var promise = this.promise;
 				// Wrap the reject procedure inside a function...
-				function r() {
-					promise.error_callback(reason);
-					if (next) next.reject(arg_for_next);
-					promise.pending = false;
+				function rej() {
+					var next = promise.next;
+					var arg_for_next;
+					try {
+						arg_for_next = promise.error_callback(reason);
+						if (next) {
+							if (QLite.isPromise(arg_for_next)) {
+								var got_to_resolve = false;
+								handleCallbackPromise(got_to_resolve, promise, arg_for_next);
+							} else next.reject(arg_for_next);
+						}
+					} catch (error) {
+						if (next) next.reject(error);
+					}
 				};
-				// ...and schedule execution as soon as possible
-				setTimeout(r, 0);
+				// ...and wait until client code terminates
+				setTimeout(rej, 0);
 			},
 			promise: {
 				success_callback: function () {}, // default: do nothing
 				error_callback: function () {}, // default: do nothing
 				then: function (success_callback, error_callback) {
 					// Override callbacks, if provided
-					if (success_callback && typeof success_callback === 'function') {
-						this.success_callback = success_callback;
-					}
-					if (error_callback && typeof error_callback === 'function') {
-						this.error_callback = error_callback;
-					}
+					if (success_callback) this.success_callback = success_callback;
+					if (error_callback) this.error_callback = error_callback;
 
 					// Return new chained promise
 					this.next = QLite.deferred();
@@ -62,15 +87,26 @@ function asyncMessage(message, wait) {
 }
 
 
-function foo() {
+function syncMessage(message) {
 	var deferred = QLite.deferred();
-	deferred.resolve('foo');
-	console.log('resolved');
+	deferred.resolve(message);
 	return deferred.promise;
 }
 
-foo().then(function (m) {
-	console.log(m);
+function failingAsyncMessage(message, wait) {
+	var deferred = QLite.deferred();
+	setTimeout(function () {
+		deferred.reject(message);
+	}, wait);
+	return deferred.promise;
+}
+
+asyncMessage('yo', 1000).then(function () {
+	throw 'nope';
+}).then(function (m) {
+	console.log('ok: ' + m);
+}, function (m) {
+	console.log('error: ' + m);
 });
 
 /*
